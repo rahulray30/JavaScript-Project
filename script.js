3b7c3dac3f58e3df606be74c4bfe31d98861ca5a
rahulray <rayrahul.2011@gmail.com>
2020-11-27 08:07:35 +0530

    Adding reset and random functionality

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-
 console.log("HHjhhh");
 var butAll = document.querySelectorAll("button");
 var initArr = [];
 console.log(document.getElementById("r").style.backgroundColor);
 
-
 //console.log(initArr[0].style.backgroundColor);
 
 function colourChange(cs) {
-  if (cs.value == "Purple") {
+  if (cs.value == "Reset") {
+    let colour = ["red", "yellow", "green"];
+    for (let i = 0; i < butAll.length; i++) {
+      butAll[i].style.backgroundColor = colour[i];
+    }
+    //location.reload();
+  } else if (cs.value == "Purple") {
     for (let i = 0; i < butAll.length; i++) {
       butAll[i].style.backgroundColor = "purple";
       console.log("Its purple");
@@ -23,8 +27,19 @@ function colourChange(cs) {
       butAll[i].style.backgroundColor = "blue";
       console.log("Its Blue");
     }
-  } else if (cs.value == "Reset") {
-    butAll[0].style.backgroundColor = initArr[0];
+  } else if (cs.value == "Random") {
+    randomValue();
+  }
+}
+
+function randomValue() {
+  let arr = ["red", "blue", "green", "orange", "purple", "blue"];
+  let p = 0;
+  while (p < butAll.length) {
+    let r = Math.floor(Math.random() * 6);
+    butAll[p].style.backgroundColor = arr[r];
+    console.log(arr[r]);
+    p++;
   }
 }
 
